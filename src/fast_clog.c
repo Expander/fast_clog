cbb6cbe60f6e2ae910be9519215b69d74b8922f0
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-04 16:51:37 +0200

    move C99 version of fast_clog to separate file

--- src/fast_clog.c
+++ src/fast_clog.c
@@ -1,14 +1,4 @@
-#include <complex.h>
 #include <math.h>
-
-
-static double _Complex fast_clog_c99(double _Complex z)
-{
-   const double rz = creal(z);
-   const double iz = cimag(z);
-
-   return 0.5*log(rz*rz + iz*iz) + I*atan2(iz, rz);
-}
 
 
 void fast_clog(double re, double im, double* out_re, double* out_im)
